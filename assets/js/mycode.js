f3cf2d77df8bc27255367dedff0fe7a87dc47ef5
Kyle Geske <stungeye@gmail.com>
2021-01-12 14:56:52 -0600

    Updates for lightbox and codepen integration.

--- assets/js/mycode.js
+++ assets/js/mycode.js
@@ -1,1 +1,13 @@
-console.log("this is it");
+document.addEventListener("DOMContentLoaded", function (event) {
+  if (GLightbox) {
+    GLightbox({ selector: "img" });
+  }
+
+  const codepens = document.querySelectorAll(".codepen");
+
+  if (codepens.length > 0) {
+    const codepenScript = document.createElement("script");
+    codepenScript.src = "https://static.codepen.io/assets/embed/ei.js";
+    document.head.appendChild(codepenScript);
+  }
+});
